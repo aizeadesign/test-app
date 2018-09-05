787e23af8d5870d48166af9334c17d82362bd2d5
Marion DOUMEINGTS <m.doumeingts@wks-b192.corp.dailymotion.com>
2018-09-05 18:58:17 +0200

    logos software

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,9 +5,17 @@ const IndexPage = () => (
   <div>
     <div className="Hero">
       <div className="HeroGroup">
-      <h1>Learn to design and code React apps</h1>
-      <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-      <Link to="/page-2/">Watch videos with it's title</Link>
+        <h1>Learn to <br/>design and code<br/>React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <Link to="/page-2/">Watch the video</Link>
+        <div className="Logos">
+          <img src={require('../images/logo-figma.png')} width="50" />
+          <img src={require('../images/logo-sketch.png')} width="50" />
+          <img src={require('../images/logo-invision.png')} width="50" />
+          <img src={require('../images/logo-framer.png')} width="50" />
+          <img src={require('../images/logo-react.png')} width="50" />
+          <img src={require('../images/logo-swift.png')} width="50" />
+        </div>
       </div>
     </div>
   </div>
