fa64225795e952562d44c62c7ae34ae161f0aaf8
Marion DOUMEINGTS <m.doumeingts@wks-b192.corp.dailymotion.com>
2018-09-06 18:29:21 +0200

    Card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Card from '../components/card';
 
 const IndexPage = () => (
   <div>
@@ -16,6 +17,27 @@ const IndexPage = () => (
           <img src={require('../images/logo-react.png')} width="50" />
           <img src={require('../images/logo-swift.png')} width="50" />
         </div>
+      </div>
+    </div>
+    <div className="Cards">
+      <h2>11 courses, more coming</h2>
+      <div className="CardGroup">
+        <Card 
+          title="Design System" 
+          text="10 sections"
+          image={require('../images/card1.jpg')} />
+        <Card 
+          title="React for Designers" 
+          text="12 sections"
+          image={require('../images/card2.jpg')} />
+        <Card 
+          title="Sound Design" 
+          text="5 sections"
+          image={require('../images/card4.jpg')} />
+        <Card 
+          title="ARKit 2" 
+          text="10 sections"
+          image={require('../images/card6.jpg')} />
       </div>
     </div>
   </div>
